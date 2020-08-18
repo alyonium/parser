9d32d1fe1c1ecb9a8f575ec4d601d5819042f06c
Alona Ivanova <alyonium2.0@gmail.com>
2020-08-18 16:00:30 +0300

    err429 check

--- potusScraper.js
+++ potusScraper.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const download = require('image-downloader');
+const http = 'https://wallpaperscraft.ru';
 const url = [
-    'https://wallpaperscraft.ru/catalog/nature',
-    'https://wallpaperscraft.ru/catalog/city',
-    'https://wallpaperscraft.ru/catalog/flowers'
+    `${http}/catalog/nature`,
+    `${http}/catalog/city`,
+    `${http}/catalog/flowers`
 ];
-const httpGetWallpaper = 'https://wallpaperscraft.ru/';
 
-
-const arrayOfPromises = url.map(url => {
+const getLinksWithPage = url.map(url => {
     return new Promise((resolve, reject) => {
         axios.get(url)
             .then(function (response) {
@@ -23,22 +23,55 @@ const arrayOfPromises = url.map(url => {
     })
 });
 
-Promise.all(arrayOfPromises)
-    .then(links => {
-        const newLinks = {...links};
-        console.log(newLinks)
-        links.forEach(url => {
-            axios.get(url)
-                .then(response => {
-                    const $ = cheerio.load(response.data);
-                    const countOfPicture = $('.wallpapers__image').length;
-                    const numberOfPicture = Math.floor(Math.random() * countOfPicture + 1);
-                    url = $($('.wallpapers__item .wallpapers__link')[numberOfPicture]).attr('href');
-                })
-                .catch(error => {
-                console.log(error)
+Promise.all(getLinksWithPage)
+    .then(url => {
+        const getLinksOfPictures = url.map(url => {
+            return new Promise((resolve, reject) => {
+                axios.get(url)
+                    .then(response => {
+                        const $ = cheerio.load(response.data);
+                        const countOfPicture = $('.wallpapers__image').length;
+                        const numberOfPicture = Math.floor(Math.random() * countOfPicture + 1);
+                        const path = $($('.wallpapers__item .wallpapers__link')[numberOfPicture]).attr('href').replace('wallpaper', 'download');
+                        resolve(`${http}${path}/1600x1200`);
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
             })
-            console.log(links)
-            // axios.get(httpGetWallpaper)
         });
-    })
+
+        Promise.all(getLinksOfPictures)
+            .then(url => {
+                const getLinksForDownload = url.map(url => {
+                    return new Promise((resolve, reject) => {
+                        axios.get(url)
+                            .then(response => {
+                                const $ = cheerio.load(response.data);
+                                const button = $('.gui-toolbar.gui-toolbar_stretch.gui-subheading .gui-toolbar__item.gui-hidden-mobile .gui-button.gui-button_full-height').attr('href');
+                                resolve(button)
+                            })
+                            .catch(error =>
+                                reject(error)
+                            )
+                    })
+                });
+
+                Promise.all(getLinksForDownload)
+                    .then(url => {
+                        url.forEach(url => {
+                            const options = {
+                                url: url,
+                                dest: 'down/study/parser/images'
+                            }
+                            download.image(options)
+                                .then(({ filename }) => {
+                                    console.log('Saved to', filename)
+                                })
+                                .catch((err) =>
+                                    console.error(err)
+                                )
+                        })
+                    })
+            });
+    });
