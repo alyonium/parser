34d1b6ba8d6aaec4bbe5ee6d410617c6340f8db1
Alona Ivanova <alyonium2.0@gmail.com>
2020-08-19 16:05:50 +0300

    work

--- potusScraper.js
+++ potusScraper.js
@@ -2,76 +2,84 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const download = require('image-downloader');
 const http = 'https://wallpaperscraft.ru';
-const url = [
+const urlList = [
     `${http}/catalog/nature`,
     `${http}/catalog/city`,
-    `${http}/catalog/flowers`
+    `${http}/catalog/flowers`,
 ];
 
-const getLinksWithPage = url.map(url => {
+//choose page for download
+const getLinksWithPage = urlList.map(url => {
     return new Promise((resolve, reject) => {
         axios.get(url)
             .then(function (response) {
                 const $ = cheerio.load(response.data);
                 const maxPage = +($('.pager__item.pager__item_last-page .pager__link').attr('href').split('/')[3].split('page')[1]);
                 const newPage = Math.floor(Math.random() * maxPage + 1);
-                resolve(`${url}/page${newPage}`)
+                resolve(`${url}/page${newPage}`);
             })
             .catch(function (error) {
                 reject(error)
-            })
-    })
+            });
+    });
 });
 
+//choose picture from page
 Promise.all(getLinksWithPage)
-    .then(url => {
-        const getLinksOfPictures = url.map(url => {
+    .then(urlList => {
+        const getLinksOfPictures = urlList.map(url => {
             return new Promise((resolve, reject) => {
                 axios.get(url)
                     .then(response => {
                         const $ = cheerio.load(response.data);
                         const countOfPicture = $('.wallpapers__image').length;
-                        const numberOfPicture = Math.floor(Math.random() * countOfPicture + 1);
+                        const numberOfPicture = Math.floor(Math.random() * countOfPicture);
                         const path = $($('.wallpapers__item .wallpapers__link')[numberOfPicture]).attr('href').replace('wallpaper', 'download');
                         resolve(`${http}${path}/1600x1200`);
                     })
                     .catch(error => {
                         reject(error)
                     })
-            })
+            });
         });
 
+        //on the page of picture search the button for download and get a link from this
         Promise.all(getLinksOfPictures)
-            .then(url => {
-                const getLinksForDownload = url.map(url => {
-                    return new Promise((resolve, reject) => {
+            .then(urlList => {
+                const getLinksForDownload = urlList.map(url => {
+                    return new Promise((resolve) => {
                         axios.get(url)
                             .then(response => {
                                 const $ = cheerio.load(response.data);
                                 const button = $('.gui-toolbar.gui-toolbar_stretch.gui-subheading .gui-toolbar__item.gui-hidden-mobile .gui-button.gui-button_full-height').attr('href');
-                                resolve(button)
+                                resolve(button);
                             })
-                            .catch(error =>
-                                reject(error)
-                            )
-                    })
+                            .catch(error => {
+                                resolve(undefined)
+                            })
+                    });
                 });
 
+                //download image
                 Promise.all(getLinksForDownload)
-                    .then(url => {
-                        url.forEach(url => {
-                            const options = {
-                                url: url,
-                                dest: 'down/study/parser/images'
+                    .then(urlList => {
+                        urlList.forEach(url => {
+                            if (url === undefined) {
+                                console.log('STATUS CODE 404');
+                            } else {
+                                const options = {
+                                    url: url,
+                                    dest: './images'
+                                }
+                                download.image(options)
+                                    .then(({ filename }) => {
+                                        console.log('Saved to', filename)
+                                    })
+                                    .catch((err) =>
+                                        console.error(err)
+                                    )
                             }
-                            download.image(options)
-                                .then(({ filename }) => {
-                                    console.log('Saved to', filename)
-                                })
-                                .catch((err) =>
-                                    console.error(err)
-                                )
-                        })
-                    })
+                        });
+                    });
             });
     });
